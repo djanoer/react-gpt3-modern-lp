54a0e31afc88cdba0bdd7f1c1a95ed07122e7e8d
dave_id <djanur405@gmail.com>
2022-02-13 16:01:47 +0800

    section container/blog dan components/article selesai

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 import './blog.css';
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container__groupA'>
+          <Article imgUrl={blog01} date='Feb 13, 2022' title='Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, soluta nostrum?' />
+        </div>
+        <div className='gpt3__blog-container__groupB'>
+          <Article imgUrl={blog02} date='Feb 13, 2022' title='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, alias earum.' />
+          <Article imgUrl={blog03} date='Feb 13, 2022' title='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, voluptatem.' />
+          <Article imgUrl={blog04} date='Feb 13, 2022' title='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, ipsum!' />
+          <Article imgUrl={blog05} date='Feb 13, 2022' title='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia.' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
