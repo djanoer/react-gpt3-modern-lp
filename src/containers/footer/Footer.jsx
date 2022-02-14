24788225ee1b68e9269d91232368b9694e2262dc
dave_id <djanur405@gmail.com>
2022-02-14 10:42:34 +0800

    section footer selesai

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
+import gpt3Logo from '../../assets/logo.svg';
 import './footer.css';
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+const Footer = () => (
+  <div className='gpt3__footer section__padding'>
+    <div className='gpt3__footer-heading'>
+      <h1 className='gradient__text'> Do you want to step in to the future before other</h1>
+    </div>
+
+    <div className='gpt3__footer-btn'>
+      <p>Request Early Access</p>
+    </div>
+
+    <div className='gpt3__footer-links'>
+      <div className='gpt3__footer-links__logo'>
+        <img src={gpt3Logo} alt='gpt3-logo' />
+        <p>
+          Build up by dave_id, <br /> All Rights Reserved
+        </p>
+      </div>
+      <div className='gpt3__footer-links__div'>
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+
+      <div className='gpt3__footer-links__div'>
+        <h4>Company</h4>
+        <p>Terms & Conditions</p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+
+      <div className='gpt3__footer-links__div'>
+        <h4>Get in touch</h4>
+        <p>dave_id, Banyuwangi - Indonesia</p>
+        <p>+62 1945 641 644</p>
+        <p>dave_id@mail.com</p>
+      </div>
+    </div>
+
+    <div className='gpt3__footer-copyright'>
+      <p>@2022 GPT-3. All rights reserved.</p>
+    </div>
+  </div>
+);
 
 export default Footer;
